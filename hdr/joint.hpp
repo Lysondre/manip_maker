8ac3554c4a90932f946aecfc3f20c64bd52d9a1a
Lysondre <lissa.savard@gmail.com>
2024-06-26 07:42:42 -0400

    Added missing Joint constructor

--- hdr/joint.hpp
+++ hdr/joint.hpp
@@ -15,6 +15,11 @@ struct Joint
     double theta;  // don't care when revolute
     double d;      // don't care when prismatic
     JointType type;
+
+    Joint(double alpha = 0.0, double a = 0.0, double theta = 0.0, double d = 0.0)
+        : alpha(alpha), a(a), theta(theta), d(d)
+    {
+    }
 };
 
 #endif  // JOINT_H
